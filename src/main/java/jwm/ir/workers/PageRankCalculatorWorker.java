5b26e8a41ebd8ae42398a44e7bc151b64da44a07
Jeff Mair <jeffwmair@gmail.com>
2016-08-10 23:10:47 -0400

    cleanup of PageRankCalculatorWorker

--- src/main/java/jwm/ir/workers/PageRankCalculatorWorker.java
+++ src/main/java/jwm/ir/workers/PageRankCalculatorWorker.java
@@ -42,101 +42,37 @@ class PageRankCalculatorWorker implements Runnable {
 
 		// get the verticies of the graph
 		long start = System.currentTimeMillis();
-		log.info("Beginning to get pageIds for calculating pageRank");
 
-		log.warn("todo: fix this because its stupid");
 		List<Page> pages = service.getAllPages();
+		int edgeNumber = 1;
 		for(Page page : pages) {
 			_graph.addVertex(Long.toString(page.getId()));
-			log.debug("Page:"+page);
-		}
-
-		log.info("Finished getting pageIds for calculating pageRank (" + ((System.currentTimeMillis() - start) / 1000.0) + "s)");
-
-
-		// now get the edges (out links)
-		int k = 0;
-		int setSize = 500000;	// max size of sets of page ids to request links of
-		int edgeNumber = 1;
-		start = System.currentTimeMillis();
-		log.info("Beginning to get page-links for calculating pageRank");
-		boolean notDone = true;
-//		while(notDone) {
-
-
-			/**
-			 *
-			 * todo: don't need this loop anymore either
-			 * Get Page objects back above, with link to PageLink collection (add to entity).
-			 * Then we have everything here.
-
-			ArrayList<String> pageIdsToRequestLinks = new ArrayList<>();
-			for(int j = 0; j < setSize; j++) {
-				if (j+k >= allPageIds.size()-1) {
-					notDone = false;
-					break;
-				}
-				else {
-					pageIdsToRequestLinks.add(allPageIds.get(k+j));
-				}
-			}
-			 */
-
-//			log.debug("Get pagelinks for:"+pageIdsToRequestLinks);
-//			List<String> pageIdDestIds = _db.getPageLinks(pageIdsToRequestLinks);
-//			log.debug("Got pagelinks:"+pageIdDestIds);
-//			if (pageIdDestIds.isEmpty()) {
-//				log.error("No pagelinks were found!!  Need to fix this");
-//			}
-//			for (String pageIdDestId : pageIdDestIds) {
-//				String[] page_dest = pageIdDestId.split(",");
-//				_graph.addEdge(edgeNumber++, page_dest[0], page_dest[1]);
-//			}
-		for(Page p : pages) {
-			for (PageLink edge : p.getPageLinks()) {
+			for (PageLink edge : page.getPageLinks()) {
 				String edgeStart = Long.toString(edge.getPage().getId());
 				String edgeEnd = Long.toString(edge.getDestinationPage().getId());
-				log.debug("Adding edge.  Start:'"+edgeStart+"', End:'"+edgeEnd+"'");
 				_graph.addEdge(edgeNumber++, edgeStart, edgeEnd);
 			}
 		}
 
-//			k += setSize;
-//		}
-		log.info("Finished getting page-links for calculating pageRank  (" + ((System.currentTimeMillis() - start) / 1000.0) + "s)");
-
 		// calculate PR
-		log.info("Beginning to calculate PageRank on " + pages.size() + " pages");
 		start = System.currentTimeMillis() ;
 		PageRank<String, Integer> ranker = new PageRank<>(_graph, ALPHA);
 		ranker.setTolerance(TOLERANCE) ;
 		ranker.setMaxIterations(MAX_ITERATIONS);
 		ranker.evaluate();
-		log.info("Finished calculating PageRank in " + (System.currentTimeMillis()-start) / 1000.0 + "s");
+		log.info("Finished calculating PageRank in " + (System.currentTimeMillis()-start) / 1000.0 + "s on "+pages.size()+" pages");
 
 		double prSum = 0.0;
-		int maxPrsToSendAtOnce = 50000;
-		log.info("Beginning to send PageRanks to the Database");
 		HashMap<Integer, Double> pageRanks = new HashMap<>();
 		for(int i = 1; i <= pages.size(); i++) {
 			String pageId = Long.toString(pages.get(i-1).getId());
 			double pr = ranker.getVertexScore(pageId);
 			log.debug("Got vector score for page '"+pageId+"':"+pr);
 			pageRanks.put(Integer.parseInt(pageId), pr);
-			if (i % maxPrsToSendAtOnce == 0) {
-				// send 'em
-				_db.updatePageRanks(pageRanks);
-				log.info("A batch of PageRanks was sent to the Database");
-				pageRanks.clear();
-			}
-			else {
-				log.debug("not sending to db yet...");
-			}
 			prSum += pr;
 		}
 
 		_db.updatePageRanks(pageRanks);
-		pageRanks.clear();
 		log.info("Finished calculating PageRank on all pages; sum of all PR scores was: " + prSum);
 	}
 
