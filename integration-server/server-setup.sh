461d311bfc5b41a849caa9df3f4a718916907e78
Jeff Mair <jeffwmair@gmail.com>
2016-08-30 21:50:38 -0400

    removed unused mysql commands

--- integration-server/server-setup.sh
+++ integration-server/server-setup.sh
@@ -7,7 +7,4 @@ sudo apt-get install default-jdk -y
 echo "JAVA_HOME=/usr/bin/java" >> /etc/environment
 sudo apt-get install git -y
 sudo apt-get install maven -y
-
 sudo apt-get install mysql-server -y
-sudo mysql_secure_installation
-sudo mysql_install_db
