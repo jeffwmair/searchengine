cf7685269333381642e2e8688687deb29807d5c0
Jeff Mair <jeffwmair@gmail.com>
2016-08-30 22:07:32 -0400

    auto-yes for jenkins installation

--- integration-server/server-setup.sh
+++ integration-server/server-setup.sh
@@ -10,5 +10,5 @@ sudo apt-get install maven -y
 sudo apt-get install mysql-server -y
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
