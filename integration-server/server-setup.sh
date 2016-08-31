8928cb1704f4ebadaf20af985af7ba55e07e50ba
Jeff Mair <jeffwmair@gmail.com>
2016-08-30 21:59:58 -0400

    jenkins

--- integration-server/server-setup.sh
+++ integration-server/server-setup.sh
@@ -8,3 +8,7 @@ echo "JAVA_HOME=/usr/bin/java" >> /etc/environment
 sudo apt-get install git -y
 sudo apt-get install maven -y
 sudo apt-get install mysql-server -y
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install jenkins
