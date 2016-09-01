e720a4e42bc864e2dbea88be1e1aca34d92c9fb9
Jeff Mair <jeffwmair@gmail.com>
2016-09-01 14:50:45 -0400

    Update server-setup.sh

--- integration-server/server-setup.sh
+++ integration-server/server-setup.sh
@@ -29,6 +29,7 @@ mysql --execute="create user 'se_test_user'@'localhost' identified by 'se_test_u
 mysql --execute="grant all on searchengine_test.* to 'se_test_user'@'localhost';"
 
 sudo apt-get install tomcat7 -y
+sudo apt-get install tomcat7-docs tomcat7-admin tomcat7-examples -y
 sed -i -e 's/8080/8081/g' /var/lib/tomcat7/conf/server.xml
 sed -i -e 's/JAVA_OPTS="-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC"/JAVA_OPTS="-Djava.security.egd=file:\/dev\/.\/urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"/g' /etc/default/tomcat7
 sudo service tomcat7 restart
